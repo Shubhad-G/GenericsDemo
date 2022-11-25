ca4e93cd74cbd3dd5d7b80a33925797160d04219
Shubhad Gonal <shubhadgonal19@gmail.com>
2022-11-25 15:40:19 +0530

    Generics demo usecase 1,simple program implementation without generics

---
+++ GenericsDemo/GenericsMethods.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsDemo
+{
+    internal class GenericsMethods
+    {
+        public static void printIntArray(int[] intArray)
+        {
+            foreach(int i in intArray)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("-------------\n");
+        }
+
+        public static void printDoubleArray(double[] doubleArray)
+        {
+            foreach(double i in doubleArray)
+            {
+                System.Console.WriteLine(i);
+            }
+            Console.WriteLine("-------------\n");
+        }
+        public static void printCharArray(char[] charArray)
+        {
+            foreach(char i in charArray)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("-------------\n");
+        }
+    }
+}
---
+++ GenericsDemo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] intArray = { 1,2,3,4,5};
+            double[] doubleArray = { 1.2, 2.2, 3.2, 4.2 };
+            char[] charArray = { 'a', 'b', 'c', 'd', 'e' };
+
+            GenericsMethods.printIntArray(intArray);
+            GenericsMethods.printDoubleArray(doubleArray);
+            GenericsMethods.printCharArray(charArray);
+
+            Console.ReadLine();
+            
+        }
+    }
+}
